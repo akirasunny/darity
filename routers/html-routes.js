08af55138dff064bd2ffd3dacbd70eeeeb927ab9
David Lac <kiddmit3@gmail.com>
2017-09-01 12:39:22 -0700

    working

---
+++ routers/html-routes.js
@@ -0,0 +1,48 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+app.get("/", function(req, res) {
+    res.render("index");
+  });
+
+app.get("/about", function(req, res) {
+    res.render("about");
+  });
+
+app.get("/faqs", function(req, res) {
+    res.render("faqs");
+  });
+
+app.get("/leaderboard", function(req, res) {
+    res.render("leaderboard");
+  });
+
+app.get("/myprofile", function(req, res) {
+    res.render("myprofile");
+  });
+
+app.get("/players", function(req, res) {
+    res.render("players");
+  });
+
+app.get("/portal", function(req, res) {
+    res.render("portal");
+  });
+
+app.get("/sponsors", function(req, res) {
+    res.render("sponsors");
+  });
+
+};
+
+
+//
