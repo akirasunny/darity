641a841378985e7ea80221b633b27d2f9c342c21
SueAkira <Akira@SueAkiradeMacBook-Air.local>
2017-09-06 10:43:46 -0700

    server side updated

--- routers/sponsor-routes.js
+++ routers/sponsor-routes.js
@@ -1,4 +1,5 @@
-var db = require("../models")
+var db = require("../models");
+var sha1 = require("sha1");
 
 module.exports = function(app) {
 	app.get("/api/sponsors", function(req, res) {
@@ -8,4 +9,24 @@ module.exports = function(app) {
 			res.json(data);
 		});
 	});
+
+	app.post("/newsponsor", function(req, res) {
+		db.sponsors.create({
+			name: req.body.name,
+			email: sha1(req.body.email)
+		})
+	});
+
+	app.post("/sponsor-login", function(req, res) {
+		var username = req.body.name;
+		var email = req.body.email;
+		db.sponsors.findOne({
+			where: {
+				name: username,
+				email: email
+			}
+		}).then(function(data) {
+			res.redirect("/sponsor");
+		})
+	})
 }
